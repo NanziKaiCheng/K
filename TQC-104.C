b9ae6cff528e23411f620581b58e1770d1a9a4e6
NanziKaiCheng <48470730+NanziKaiCheng@users.noreply.github.com>
2019-04-01 19:11:26 +0800

    Update TQC-104.C

--- TQC-104.C
+++ TQC-104.C
@@ -3,13 +3,13 @@
 
 int main() 
 {
-     int unit;
-     double price=23.34; 
-     double total;
+     int unit; //宣告一個變數
+     double price=23.34; //宣告price=23.34
+     double total; //宣告total是雙精度符標點
      printf("請問您要買幾瓶蘋果汁? ");
-     scanf("%d", &unit);
+     scanf("%d", &unit); //輸入一個數存入unit
      total = unit*price;
      printf("我買了%d瓶100%的蘋果汁\n", unit);
-     printf("花了%lf元", total);
+     printf("花了%lf元", total); //因為是用double所以要用lf ;float是用f
      return 0;
 }
