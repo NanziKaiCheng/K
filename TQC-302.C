1ee2b81d77fe74bdb9ecba9a1793c302d03357f2
NanziKaiCheng <48470730+NanziKaiCheng@users.noreply.github.com>
2019-04-21 01:05:54 +0800

    Update TQC-302.C

--- TQC-302.C
+++ TQC-302.C
@@ -3,11 +3,11 @@
 int adjust(int); //宣告函式
 int main () 
 {
-     int score, final;
+     int score, final; //宣告2個變數
      printf("請輸入您的分數: ");
      scanf("%d", &score);
 
-     final= adjust(score); //final=算完之後的分數
+     final= adjust(score); //final=算完之後的分數 adjust(score)好像是一個函數
 
      printf("調整後的分數: %d", final);
      
@@ -16,8 +16,8 @@ int main ()
 
 int adjust(int score) //把輸入得數帶來這裡
 {
-	int temp;
-     if (score >= 60) 
+	int temp;  //宣告一個變數
+     if (score >= 60)   
           temp = score + 5;
      else 
           temp = score + 10;
